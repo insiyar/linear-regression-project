1cb287e88d70c428ee0c4cc1c3c24c0b4df9b83c
AnanyaM12 <ananyaamathur03@gmail.com>
2022-11-17 13:15:56 +0530

    code

---
+++ Linear regression project.R
@@ -0,0 +1,41 @@
+#reading the csv file
+data<-read.csv(c("C:/Users/dell/Desktop/Marketing_Data.csv"))
+print(data)
+
+set.seed(123)#random sampling
+
+# creating training data as 80% of the dataset
+a_sample <- sort(sample(nrow(data),nrow(data)*0.8))
+
+#training data set
+training_dataset  <- c(data[a_sample, ])
+
+#testing data set
+testing_dataset <- c(data[-a_sample, ])
+
+#making the model
+model <-lm(sales~.,data=training_dataset)
+print(model)
+
+
+#plotting all the three variables 
+library(car)
+plot(model)
+abline(model)
+
+
+#prediction of the testing dataset 
+testing_dataset <- data[-a_sample,] 
+y_pred <- predict(model, newdata=testing_dataset)
+print(y_pred)
+
+
+#cross-validation by calculating RMSE,R^2,MAE
+
+library(Metrics)
+RMSE = rmse(data$sales,predict(model, newdata=testing_dataset))
+print(RMSE)
+R2=summary(model)$r.squared
+print(R2)
+MAE=mae(data$sales,predict(model, newdata=testing_dataset))
+print(MAE)
